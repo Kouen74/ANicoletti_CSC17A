228c754eceb0675680ba49f77e905175d6d07c30
Kouen74 <grenadesnham@gmail.com>
2018-03-08 20:22:30 -0800

    Add files via upload

---
+++ BinaryStringSearch.cpp
@@ -0,0 +1,49 @@
+
+/* 
+ * File:  BinaryStringSearch.cpp
+ * Author: Andrew Nicoletti
+ * Created on March 8, 2018, 07:05 PM
+ * Purpose: Template
+ */
+ 
+#include <iostream>     // System Library
+#include <string>
+
+using namespace std;    // Standard I/O Namespace
+
+int search(const int[], int, int) {}
+
+
+int main(int argc, char** argv) {
+   
+    // Declare Variables
+    const int NUM_NAMES = 20;
+    string lastname, result, names[NUM_NAMES] = {"Allison, Jeff", "Allen, Jim", "Collins, Bill", "Griffin, Jim", "Harrison, Rose", "Holland, Beth",
+                                "James, Jean", "Javens, Renee", "Johnson, Jill",  "Looney, Joe", "Pike, Gordon", "Pore, Bob",
+                                "Rose, Geri", "Rutherford, Greg", "Setzer, Cathey", "Smith, Bart", "Stamey, Marty", "Taylor, Terri",
+                               "Weaver, Jim", "Wolfe, Bill" };            //insert your code to complete this program
+    // Input Data
+    cout<<"Input the last name of the employee"<<endl;
+    cin>>lastname;
+    // Process Data
+    result = search(names, NUM_NAMES, lastname);
+    // Output Data
+    if (result==-1) cout<<"Name not found"<<endl; else { cout<<"Name found, "<<endl;
+    
+    return 0;
+}
+
+int search(const int array[], int size, int name) {
+    int first = 0,
+            last = size - 1,
+            middle,
+            position = -1;
+    bool found = false;
+    
+    while(!found && first <= last){
+        middle = (first+last)/2;
+        if (array[middle]==name) found = true, position = middle;
+    }  if (array[middle] > name) last = middle-1;
+    else first = middle+1;
+    
+}
